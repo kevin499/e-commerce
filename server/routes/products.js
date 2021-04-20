fd3105321d8681e1b93cfeae741d22fec3272190
Kevin <33708864+kevin499@users.noreply.github.com>
2021-04-20 16:44:02 -0300

    adding middleware

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,18 +1,20 @@
 const express = require('express')
 
+const { is_admin } = require("../middlewares/is_admin")
+
 const productController = require('../controllers/productController')
 
 const router_product = express.Router()
 
 
-router_product.get("/", productController.index)
+router_product.get("/",productController.index)
 
-router_product.post("/",  productController.store)
+router_product.post("/", is_admin, productController.store)
 
-router_product.get("/:id",  productController.show)
+router_product.get("/:id", productController.show)
 
-router_product.put("/:id",  productController.update)
+router_product.put("/:id", is_admin, productController.update)
 
-router_product.delete("/:id",  productController.destroy)
+router_product.delete("/:id", is_admin, productController.destroy)
 
 module.exports = router_product
--- server/routes/shopping_cart.js
+++ server/routes/shopping_cart.js
@@ -1,18 +1,20 @@
 const express = require('express')
 
-const router_shopping_cart = express.Router()
+const { is_admin } = require("../middlewares/is_admin")
 
 const shoppingCartController = require('../controllers/shoppingCartController')
+
+const router_shopping_cart = express.Router()
 
 
 router_shopping_cart.get("", shoppingCartController.index)
 
 router_shopping_cart.get("/:id", shoppingCartController.show)
 
-router_shopping_cart.post("", shoppingCartController.store)
+router_shopping_cart.post("", is_admin, shoppingCartController.store)
 
-router_shopping_cart.put("/:id", shoppingCartController.update)
+router_shopping_cart.put("/:id", is_admin, shoppingCartController.update)
 
-router_shopping_cart.delete("/:id", shoppingCartController.destroy)
+router_shopping_cart.delete("/:id", is_admin, shoppingCartController.destroy)
 
 module.exports = router_shopping_cart
